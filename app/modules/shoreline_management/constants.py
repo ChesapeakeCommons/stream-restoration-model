fa5d8f601fb8f044174133d84030e644fe2d33a1
brendanmcintyre <brendan.david.mcintyre@gmail.com>
2019-12-17 13:04:17 -0500

    Added mathematical functions to `shoreline_management` module

--- app/modules/shoreline_management/constants.py
+++ app/modules/shoreline_management/constants.py
@@ -25,3 +25,24 @@ INPUT_DEFAULTS = {
     'bank_instability_reduction_factor': 1.0,
     'planted_tidal_wetland_area': 0.25
 }
+
+STATE_COEFFICIENTS = {
+    'n': {
+        'dc': lambda a: a * 0.04756,
+        'de': lambda a: a * 0.04756,
+        'md': lambda a: a * 0.04756,
+        'va': lambda a: a * 0.01218
+    },
+    'p': {
+        'dc': lambda a: a * 0.03362,
+        'de': lambda a: a * 0.03362,
+        'md': lambda a: a * 0.03362,
+        'va': lambda a: a * 0.00861
+    },
+    'tss': {
+        'dc': lambda a: (164*a)/2000,
+        'de': lambda a: (164*a)/2000,
+        'md': lambda a: (164*a)/2000,
+        'va': lambda a: (42*a)/2000
+    }
+}
--- app/modules/shoreline_management/utilities.py
+++ app/modules/shoreline_management/utilities.py
@@ -18,56 +18,249 @@ limitations under the License.
 
 # Import standard dependencies
 
-import math
-from datetime import datetime
+import functools
+import operator
 
 # Import package dependencies
 
 from app import logger
 
+# Import module dependencies
+
+from .constants import STATE_COEFFICIENTS
+
+
+if not hasattr(__builtins__, 'basestring'):
+
+    basestring = (str, bytes)
+
+
+# Protocol 1: Erosion Prevention
+
+
+def tn_1(*args):
+    """
+    Multiply a list of values and multiply the product
+    by a standard coefficient.
+
+    :param args: A list of numeric values representing
+    variables associated with the physical conditions
+    affecting a shoreline management conservation
+    practice.
+    :return: A float value representing the estimated
+    total nitrogen load reduction in pounds produced
+    by the given input variable values.
+    """
+
+    product = functools.reduce(operator.mul, args)
+
+    return product * 0.00029
+
+
+def tp_1(*args):
+    """
+    Multiply a list of values and multiply the product
+    by a standard coefficient.
+
+    :param args: A list of numeric values representing
+    variables associated with the physical conditions
+    affecting a shoreline management conservation
+    practice.
+    :return: A float value representing the estimated
+    total phosphorus load reduction in pounds produced
+    by the given input variable values.
+    """
+
+    product = functools.reduce(operator.mul, args)
+
+    return product * 0.000205
+
+
+def tss_1(*args):
+    """
+    Multiply a list of values and divide the product
+    by 2,000 (pounds per ton).
+
+    :param args: A list of numeric values representing
+    variables associated with the physical conditions
+    affecting a shoreline management conservation
+    practice.
+    :return: A float value representing the estimated
+    total suspended solids load reduction in tons
+    produced by the given input variable values.
+    """
+
+    product = functools.reduce(operator.mul, args)
+
+    return product / 2000
+
+
+# Protocol 2: Denitrification
+
+
+def tn_2(area):
+    """
+    Multiply an area value (acres) by a standard coefficient.
+
+    :param area: A float value representing the area
+    of tidal marsh plantings associated with a
+    shoreline management conservation practice.
+    :return: A float value representing the estimated
+    total nitrogen load reduction in pounds produced
+    by the given area variable input.
+    """
+
+    return 85.0 * area
+
+
+# Protocol 3: Sedimentation
+
+
+def tp_3(area):
+    """
+    Multiply an area value (acres) by a standard coefficient.
+
+    :param area: A float value representing the area
+    of tidal marsh plantings associated with a
+    shoreline management conservation practice.
+    :return: A float value representing the estimated
+    total phosphorus load reduction in pounds produced
+    by the given area variable input.
+    """
+
+    return 5.289 * area
+
+
+def tss_3(area):
+    """
+    Multiply an area value (acres) by a standard coefficient.
+
+    :param area: A float value representing the area
+    of tidal marsh plantings associated with a
+    shoreline management conservation practice.
+    :return: A float value representing the estimated
+    total suspended solids load reduction in tons
+    produced by the given area variable input.
+    """
+
+    return 3.4795 * area
+
+
+# Protocol 4: Marsh Redfield Ratio
+
+
+def tn_4(area):
+    """
+    Multiply an area value (acres) by a standard coefficient.
+
+    :param area: A float value representing the area
+    of tidal marsh plantings associated with a
+    shoreline management conservation practice.
+    :return: A float value representing the estimated
+    total nitrogen load reduction in pounds produced
+    by the given area variable input.
+    """
+
+    return 6.83 * area
+
+
+def tp_4(area):
+    """
+    Multiply an area value (acres) by a standard coefficient.
+
+    :param area: A float value representing the area
+    of tidal marsh plantings associated with a
+    shoreline management conservation practice.
+    :return: A float value representing the estimated
+    total phosphorus load reduction in pounds produced
+    by the given area variable input.
+    """
+
+    return 0.3 * area
+
 
 def reduction(data):
-
-    """If the measurement_period is Pre-Installation the LER will use the
-    raw lateral_erosion_rate provided by the user.
-
-    If the measurement_period is Planning or Installation the LER will be
-    halved (i.e., multipled by 0.5).
-
-    The Expert Panel (EP) guidance specifies a default value of 50%,
-    subject to post-installation monitoring which could justify a larger
-    reduction efficiency.[1]
-
-    [1] Gene Yagow, Senior Research Scientist, Biological Systems
-        Engineering Department Virginia Tech
     """
-    lateral_erosion_rate = data.get('lateral_erosion_rate', 0) if data.get('lateral_erosion_rate', 0) else 0
-    length_of_streambank = data.get('length_of_streambank', 0) if data.get('length_of_streambank', 0) else 0
-    soil_bulk_density = data.get('soil_bulk_density', 0) if data.get('soil_bulk_density', 0) else 0
+    Extract model parameters (using defaults if necessary)
+    and perform metric-specific calculations.
 
-    ler = lateral_erosion_rate * 0.5
+    :param data: A dictionary that may contain a full set of
+    float values representing variables associated with the
+    physical conditions affecting a shoreline management
+    conservation practice.
+    :return: A dictionary containing calculated model values
+    for each target metric.
+    """
 
-    base_length = length_of_streambank
-    soil_density = soil_bulk_density
+    has_majority_design_completion = data.get('has_majority_design_completion', False)
 
-    eroding_bank_height = data.get('eroding_bank_height', 0) if data.get('eroding_bank_height', 0) else 0
+    length_of_living_shoreline = data.get('length_of_living_shoreline', 1000.0)
 
-    square_root = math.sqrt(eroding_bank_height * eroding_bank_height)
+    existing_avg_bank_height = data.get('existing_avg_bank_height', 4.0)
 
-    load_total = base_length * square_root * ler * soil_density
+    existing_shoreline_recession_rate = data.get('existing_shoreline_recession_rate', 1.0)
 
-    soil_n_content = data.get('soil_n_content', 0) if data.get('soil_n_content', 0) else 0
-    soil_p_content = data.get('soil_p_content', 0) if data.get('soil_p_content', 0) else 0
+    soil_bulk_density = data.get('soil_bulk_density', 93.6)
 
-    return {
-        'tn_lbs_reduced': ((load_total) / 2000) * soil_n_content,
-        'tp_lbs_reduced': ((load_total) / 2000) * soil_p_content,
-        'tss_tons_reduced': (load_total) / 2000
-    }
+    sand_reduction_factor = data.get('sand_reduction_factor', 0.551)
 
+    bank_instability_reduction_factor = data.get('bank_instability_reduction_factor', 1.0)
 
-def miles_of_streambank_restored(data):
+    planted_tidal_wetland_area = data.get('planted_tidal_wetland_area', 0.25)
 
-    length_of_streambank = data.get('length_of_streambank', 0) if data.get('length_of_streambank', 0) else 0
+    if has_majority_design_completion:
 
-    return (length_of_streambank / 5280)
+        return {
+            'tn_lbs_reduced_1': tn_1([
+                length_of_living_shoreline,
+                existing_avg_bank_height,
+                existing_shoreline_recession_rate,
+                soil_bulk_density,
+                bank_instability_reduction_factor
+            ]),
+            'tn_lbs_reduced_2': tn_2(planted_tidal_wetland_area),
+            'tn_lbs_reduced_4': tn_4(planted_tidal_wetland_area),
+            'tp_lbs_reduced_1': ([
+                length_of_living_shoreline,
+                existing_avg_bank_height,
+                existing_shoreline_recession_rate,
+                soil_bulk_density,
+                sand_reduction_factor,
+                bank_instability_reduction_factor
+            ]),
+            'tp_lbs_reduced_3': tp_3(planted_tidal_wetland_area),
+            'tp_lbs_reduced_4': tp_4(planted_tidal_wetland_area),
+            'tss_tons_reduced_1': tss_1([
+                length_of_living_shoreline,
+                existing_avg_bank_height,
+                existing_shoreline_recession_rate,
+                soil_bulk_density,
+                sand_reduction_factor,
+                bank_instability_reduction_factor
+            ]),
+            'tss_tons_reduced_3': tss_3(planted_tidal_wetland_area)
+        }
+
+    else:
+
+        state_code = data.get('state_code')
+
+        if isinstance(state_code, basestring):
+
+            state_code.lower()
+
+            if state_code in ['dc', 'de', 'md', 'va']:
+
+                n_func = STATE_COEFFICIENTS.get('n').get(state_code)
+
+                p_func = STATE_COEFFICIENTS.get('p').get(state_code)
+
+                tss_func = STATE_COEFFICIENTS.get('tss').get(state_code)
+
+                return {
+                    'tn_lbs_reduced': n_func(length_of_living_shoreline),
+                    'tp_lbs_reduced': p_func(length_of_living_shoreline),
+                    'tss_tons_reduced': tss_func(length_of_living_shoreline)
+                }
+
+    return {}
