d0d0017bead4883a55ef60decc338106679bacfe
brendanmcintyre <brendan.david.mcintyre@gmail.com>
2021-09-16 12:37:12 -0400

    Updated denitrification calculations

--- app/modules/denitrification/utilities.py
+++ app/modules/denitrification/utilities.py
@@ -30,171 +30,63 @@ from app import logger
 from .constants import URBAN_STATE_UAL as load_data
 
 
-def efficiency(key):
-    
-    idx = {
-        'n_eff': 0.2,
-        'p_eff': 0.3,
-        's_eff': 0.2
-    }
+def base_tn(value):
 
-    return idx.get(key, 0)
+    operands = [
+        2.69,
+        10e-3,
+        value,
+    ]
+
+    return reduce(lambda x, y: x*y, operands)
 
 
 def reduction(data):
 
-    '''If the measurement_period is Pre-Installation the LER will use the
-    raw installation_lateral_erosion_rate provided by the user.
+    floodplain_sq_ft = data.get('floodplain_sq_ft', 0)
 
-    If the measurement_period is Planning or Installation the LER will be
-    halved (i.e., multipled by 0.5).
+    channel_sq_ft = data.get('channel_sq_ft', 0)
 
-    The Expert Panel (EP) guidance specifies a default value of 50%,
-    subject to post-installation monitoring which could justify a larger
-    reduction efficiency.[1]
+    # Baseflow reduction factor
 
-    [1] Gene Yagow, Senior Research Scientist, Biological Systems
-        Engineering Department Virginia Tech
-    '''
-    if data.get('has_majority_design_completion', False):
+    brf = data.get('brf', 0)
+
+    # Floodplain height factor
+
+    fhf = data.get('fhf', 0)
+
+    # Aquifer conductivity reduction factor
+
+    acrf = data.get('acrf', 0)
+
+    values = [
+        floodplain_sq_ft,
+        channel_sq_ft,
+        brf,
+        fhf,
+        acrf
+    ]
+
+    if not all(isinstance(x, (float, int)) for x in values):
+
+        return {}
+
+    floodplain_tn = base_tn(floodplain_sq_ft)
+
+    channel_tn = base_tn(channel_sq_ft)
+
+    discount_factors = [brf, fhf, acrf]
+
+    denominator = reduce(lambda x, y: x*y, discount_factors)
+
+    try:
+
+        total = (floodplain_tn / denominator) + (channel_tn / denominator)
 
         return {
-            'tn_lbs_reduced': nitrogen_protocol_2(data)
+            'tn_lbs_reduced': total
         }
+    
+    except (ValueError, ZeroDivisionError):
 
-    else:
-
-        return {
-            'tn_lbs_reduced': nitrogen(data),
-            'tp_lbs_reduced': phosphorus(data),
-            'tss_tons_reduced': sediment(data)
-        }
-
-
-def bank_height_ratio(bank_height, bankfull_height):
-
-    behi = 0
-
-    if bank_height:
-
-        behi = (bank_height / bankfull_height)
-
-    return behi
-
-
-def nitrogen_protocol_2(data):
-
-    project_left_bank_height = data.get('project_left_bank_height', 0)
-    left_bank_bankfull_height = data.get('left_bank_bankfull_height', 0)
-
-    project_right_bank_height = data.get('project_right_bank_height', 0)
-    right_bank_bankfull_height = data.get('right_bank_bankfull_height', 0)
-
-    hyporheic_box_depth = data.get('hyporheic_box_depth', 0)
-
-    if hyporheic_box_depth > 5:
-
-        hyporheic_box_depth = 5
-
-    bulk_density = data.get('bulk_density_of_soil_in_hyporheic_zone ', 125)
-
-    nitrogen = 0
-    left_behi = bank_height_ratio(project_left_bank_height, left_bank_bankfull_height)
-    right_behi = bank_height_ratio(project_right_bank_height, right_bank_bankfull_height)
-    left_bank = 0
-    right_bank = 0
-    days_per_year = 365
-    coefficient = 0.000195
-
-    '''NOVEMBER 2017 MDNR UPDATES
-
-    1. CHANGE CO-EFFICIENT FROM 0.000195 to 0.075
-    2. Change the BEHI+
-
-    '''
-
-    # '=stream length * (stream width + 10) * 5 * bulk density / 2000 lbs/ton * 0.000195 lbs/ton/day * 365 days/yr
-
-    # '=stream length * (stream width + 10) * hyporheic box ft depth * bulk density / 2000 lbs/ton * 0.000195 lbs/ton/day * 365 days/yr
-
-    length_of_left_bank_with_improved_connectivity = data.get('length_of_left_bank_with_improved_connectivity', 0)
-    length_of_right_bank_with_improved_connectivity = data.get('length_of_right_bank_with_improved_connectivity', 0)
-    stream_width_at_mean_base_flow = data.get('stream_width_at_mean_base_flow', 0)
-
-    if left_behi < 1.1:
-
-        left_bank = length_of_left_bank_with_improved_connectivity * (stream_width_at_mean_base_flow / 2 + 5)
-
-    if right_behi < 1.1:
-
-        right_bank = length_of_right_bank_with_improved_connectivity * (stream_width_at_mean_base_flow / 2 + 5)
-
-    nitrogen = ((left_bank + right_bank) * hyporheic_box_depth * bulk_density / 2000) * coefficient * days_per_year
-
-    return nitrogen
-
-
-def nitrogen_protocol_3(data):
-
-    _eff = efficiency('n_eff')
-
-    watershed_impervious_area = data.get('watershed_impervious_area', 0)
-        
-    return _eff * (watershed_impervious_area * load_data['impervious']['tn_ual'] + watershed_impervious_area * load_data['pervious']['tn_ual'])
-
-
-def phosphorus_protocol_3(data):
-
-    _eff = efficiency('p_eff')
-
-    watershed_impervious_area = data.get('watershed_impervious_area', 0)
-        
-    return _eff * (watershed_impervious_area * load_data['impervious']['tp_ual'] + watershed_impervious_area * load_data['pervious']['tp_ual'])
-
-
-def sediment_protocol_1(data):
-
-    _eff = efficiency('s_eff')
-
-    watershed_impervious_area = data.get('watershed_impervious_area', 0)
-        
-    return _eff * (watershed_impervious_area * load_data['impervious']['tss_ual'] + watershed_impervious_area * load_data['pervious']['tss_ual'])
-
-
-def nitrogen(data):
-
-    reduction_ = 0
-
-    linear_feet = data.get('linear_feet')
-
-    if isinstance(linear_feet, (float, int)):
-
-        reduction_ = linear_feet * 0.075
-
-    return reduction_
-
-
-def phosphorus(data):
-
-    reduction_ = 0
-
-    linear_feet = data.get('linear_feet')
-
-    if isinstance(linear_feet, (float, int)):
-
-        reduction_ = linear_feet * 0.068
-
-    return reduction_
-
-
-def sediment(data):
-
-    reduction_ = 0
-
-    linear_feet = data.get('linear_feet')
-
-    if isinstance(linear_feet, (float, int)):
-
-        reduction_ = linear_feet * 248.0
-
-    return reduction_ / 2000.0
+        return {}
